4257192d90c20d22b982da167c20e8089301bb86
Eddy Lu <eddy.lu@mail.mcgill.ca>
2017-02-16 02:12:47 -0800

    Add company summary tile to Companies page
    Configure content tiles on Companies page to display only if query exists

--- Companies.php
+++ Companies.php
@@ -13,6 +13,14 @@
 				box-shadow: 0px 0px 5px #CCCCCC;
 			}
 
+            #tile_rendered {
+                display: none;
+                background: #FFFFFF;
+				padding: 0px;
+				margin: 0px 0px 15px 0px;
+				box-shadow: 0px 0px 5px #CCCCCC;
+            }
+
             #symbol_input {
 				vertical-align: middle;
 				padding-left: 10px;
@@ -36,7 +44,12 @@
 				cursor: pointer;
 				box-sizing: border-box;
 				-moz-box-sizing: border-box;
-			}
+            }
+
+            #companySummary_listings tr td {
+                padding-bottom: 10px;
+                padding-right: 30px;
+            }
         </style>
     </head>
     <body>
@@ -51,9 +64,33 @@
                 </input>
             </div>
 
+            <div style="display: flex; flex-direction: row;">
+
             <!--div2-->
-            <div id="tile" style="display: table; width: 830px;">
+            <div id="tile" style="width: 700px;">
                 <div id="companyChart"></div>
+            </div>
+
+            <!--div3-->
+            <div id="tile_rendered" style="width: 375px; height: 315px; margin-left: 15px; padding: 0px 20px 0px 20px;">
+                <div id="companySummary">
+                    <h2 id="companySummary_header" style="margin-bottom: 0px;"></h2>
+                    <h3 id="companySummary_subheader" style="margin-top: 5px;">
+                        <span id="companySummary_lastTradePriceOnly" style="font-size: 30px;"></span>
+                        <span id="companySummary_change" style="font-size: 20px; margin-left: 10px;"></span>
+                        <span id="companySummary_changeInPercent" style="font-size: 20px; margin-left: 5px;"></span>
+                    </h3>
+                    <table id="companySummary_listings">
+                        <tr><td>Previous Close</td><td><span id="companySummary_previousClose" style="font-weight: bold;"></span></td></tr>
+                        <tr><td>Open</td><td><span id="companySummary_open" style="font-weight: bold;"></span></td></tr>
+                        <tr><td>Day Range</td><td><span id="companySummary_dayRange" style="font-weight: bold;"></span></td></tr>
+                        <tr><td>52-Week Range</td><td><span id="companySummary_yearRange" style="font-weight: bold;"></span></td></tr>
+                        <tr><td>Volume</td><td><span id="companySummary_volume" style="font-weight: bold;"></span></td></tr>
+                        <tr><td>Average Volume</td><td><span id="companySummary_averageDailyVolume" style="font-weight: bold;"></span></td></tr>
+                    </table>
+                </div>
+            </div>
+
             </div>
 
             <script src="company.js"></script>
--- company.js
+++ company.js
@@ -10,17 +10,78 @@ function textInputSubmit() {
     }
 }
 
-var query = window.location.search.substring(1);
-
-if (query.indexOf("symbol=") >= 0) {
-    query = query.substring(query.indexOf("symbol=") + 7);
-
+var getChart = function (query) {
     var chart = document.createElement("img");
-    chart.src = "http://chart.finance.yahoo.com/z?s=" + query + "&t=6m&q=l&l=on&z=l&";
+    chart.src = "http://chart.finance.yahoo.com/z?s=" + query + "&t=6m&q=l&l=on&z=l";
     chart.style.width = "100%";
 
     var companyChart = document.getElementById("companyChart");
     companyChart.appendChild(chart);
 }
 
-console.log(query);
+hasSymbol = false;
+
+var query = window.location.search.substring(1);
+if (query.indexOf("symbol=") >= 0) {
+    query = query.substring(query.indexOf("symbol=") + 7);
+    if (query.length > 0) {
+        hasSymbol = true;
+    }
+}
+
+var getCompanySummary = function (urlParam) {
+    url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + urlParam + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+    $.getJSON(url, function (data) {
+        var results = data.query.results.quote;
+
+        var name = results.Name;
+        var symbol = results.Symbol;
+        var lastTradePriceOnly = results.LastTradePriceOnly;
+        var change = results.Change;
+        var changeinPercent = results.ChangeinPercent;
+        var previousClose = results.PreviousClose;
+        var open = results.Open;
+        var bid = results.Bid;
+        var ask = results.Ask;
+        var dayRange = results.DaysRange;
+        var yearRange = results.YearRange;
+        var volume = results.Volume;
+        var averageDailyVolume = results.AverageDailyVolume;
+
+        document.getElementById("companySummary_header").innerHTML = name + " (" + symbol + ")";
+        document.getElementById("companySummary_lastTradePriceOnly").innerHTML = lastTradePriceOnly;
+        document.getElementById("companySummary_change").innerHTML = change;
+        document.getElementById("companySummary_changeInPercent").innerHTML = "(" + changeinPercent + ")";
+        document.getElementById("companySummary_previousClose").innerHTML = previousClose;
+        document.getElementById("companySummary_open").innerHTML = open;
+        document.getElementById("companySummary_dayRange").innerHTML = dayRange;
+        document.getElementById("companySummary_yearRange").innerHTML = yearRange;
+        document.getElementById("companySummary_volume").innerHTML = volume;
+        document.getElementById("companySummary_averageDailyVolume").innerHTML = averageDailyVolume;
+        
+        if (change.charAt(0) === '+') {
+            document.getElementById("companySummary_change").style.color = "#33CC33";
+            document.getElementById("companySummary_changeInPercent").style.color = "#33CC33";
+        }
+        else if (change.charAt(0) === '-') {
+            document.getElementById("companySummary_change").style.color = "#FF0000";
+            document.getElementById("companySummary_changeInPercent").style.color = "#FF0000";
+        }
+
+        if (name.length > 25) {
+            var companySummary_listings = document.getElementById("companySummary_listings");
+            for (var i = 0; i < companySummary_listings.rows.length; i++) {
+                for (var j = 0; j < companySummary_listings.rows[i].cells.length; j++) {
+                    companySummary_listings.rows[i].cells[j].style.paddingBottom = "5px";
+                }
+            }
+        }
+    });
+}
+
+if (hasSymbol) {
+    document.getElementById("tile_rendered").style.display = "block";
+    getChart(query);
+    getCompanySummary(query);
+}
+
